e906d3e11f2e5df626e5f568eb3ee39e53e2cf34
Edi Santoso <repopamor@gmail.com>
2017-10-07 10:03:35 +0700

    React Functional Component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import UsersList from './components/UsersList';
 
 
 class App extends Component {
@@ -28,11 +29,7 @@ class App extends Component {
             <br/>
             <h1>All Users </h1>
             <hr/><br/>
-            {
-              this.state.users.map((user) => {
-                return <h4 key={user.id} className="well"><strong>{ user.username }</strong> - <em>{user.created_at}</em></h4>
-              })
-            }
+            <UsersList users={this.state.users} />
           </div>
         </div>
       </div>
